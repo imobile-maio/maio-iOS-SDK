fdee4363e5bbf8b4fa1c4bd3bf70ff8644e6e49d
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2017-04-26 12:32:13 +0900

    引数にMaio.frameworkの指定を要求するようにした。

--- scripts/update-framework.sh
+++ scripts/update-framework.sh
@@ -5,4 +5,18 @@ echo "Start Maio.framework pdate script."
 readonly root=$(cd $(dirname $0)/.. && pwd)
 echo "root: $root"
 
+# 引数からMaio.frameworkを持ってくる
+
+# Maio.frameworkであるかの検証
+# - 指定先がディレクトリでなければダメ
+# - ディレクトリでも、Info.plistを直下に持っていなければダメ
+if [ $# -ne 1 ]; then
+    echo "argument error: 引数には差し替えるMaio.frameworkへのパスが必要です。" 1>&2
+    exit 1
+fi
+if ! [ -d $1 ] || ! [ -f $1/Info.plist ]  ; then
+    echo "argument error: $1: Maio.frameworkではない可能性があります。" 1>&2
+    exit 1
+fi
+
 exit 0
