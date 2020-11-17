cde8ca3b89707265482f1b80d57cadecd8fa759a
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2020-11-17 13:23:58 +0900

    Add Maio for OpenBidding

---
+++ MaioOB.xcframework/ios-arm64_armv7/MaioOB.framework/Headers/Maio.h
@@ -0,0 +1,13 @@
+//
+//  Maio.h
+//  Maio
+//
+//  Created by Kasamatsu on 2019/08/27.
+//  Copyright © 2019 Maio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <Maio/PublicHeader.h>
+
+
